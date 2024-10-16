d4ee1b95f5de8a1a02590350227b82f5813cc9cb
Rishit <rsigotia123@gmail.com>
2024-10-16 23:24:44 +0530

    Second most optimal Approach

--- LeetCode/160. Intersection of Two Linked Lists.cpp
+++ LeetCode/160. Intersection of Two Linked Lists.cpp
@@ -1,18 +1,70 @@
 // Most optimal solution
 // changing d1 and d2 will cover the extra length which is remaining.
-ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+{
+    // edge case
+    if (headA == NULL || headB == NULL)
+        return NULL;
+
+    ListNode *d1 = headA;
+    ListNode *d2 = headB;
+
+    while (d1 != d2) // if there is no intersection they will both become null at same time.
     {
-        //edge case 
-        if(headA == NULL || headB == NULL)
-            return NULL;
+        d1 = d1 == NULL ? headB : d1->next;
+        d2 = d2 == NULL ? headA : d2->next;
+    }
+    return d1;
+}
 
-        ListNode *d1 = headA;
-        ListNode *d2 = headB;
+// Second optimal approach same TC but longer code
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+{
+    // edge case
+    if (headA == NULL || headB == NULL)
+        return NULL;
 
-        while(d1 != d2) // if there is no intersection they will both become null at same time.
+    ListNode *d1 = headA;
+    ListNode *d2 = headB;
+
+    int n = 0, m = 0, difference;
+    while (d1)
+    {
+        d1 = d1->next;
+        n++;
+    }
+    while (d2)
+    {
+        d2 = d2->next;
+        m++;
+    }
+
+    d1 = headA;
+    d2 = headB;
+
+    if (m > n)
+    {
+        difference = m - n;
+        while (difference)
         {
-            d1 = d1 == NULL ? headB : d1->next;
-            d2 = d2 == NULL ? headA : d2->next;
-        }    
-        return d1;
+            d2 = d2->next;
+            difference--;
+        }
+    }
+    else
+    {
+        difference = n - m;
+        while (difference)
+        {
+            d1 = d1->next;
+            difference--;
+        }
+    }
+
+    while (d1 != d2)
+    {
+        d1 = d1->next;
+        d2 = d2->next;
+    }
+    return d1;
 }
