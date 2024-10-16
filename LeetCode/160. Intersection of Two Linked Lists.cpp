70b1a2677162bc14b26fc1aaa8590abd4c439080
Rishit <rsigotia123@gmail.com>
2024-10-16 23:26:22 +0530

    Brute force

--- LeetCode/160. Intersection of Two Linked Lists.cpp
+++ LeetCode/160. Intersection of Two Linked Lists.cpp
@@ -68,3 +68,20 @@ ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
     }
     return d1;
 }
+
+
+//BRute force
+ListNode *getIntersectionNode(ListNode *head1, ListNode *head2)
+{
+    while(head2 != NULL) {
+        node* temp = head1;
+        while(temp != NULL) {
+            //if both nodes are same
+            if(temp == head2) return head2;
+            temp = temp->next;
+        }
+        head2 = head2->next;
+    }
+    //intersection is not present between the lists return null
+    return NULL;
+}
