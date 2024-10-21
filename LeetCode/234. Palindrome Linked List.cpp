48f29ec293a0a6d214dcad32ae5c7de1e4263b5c
Rishit <rsigotia123@gmail.com>
2024-10-21 23:16:27 +0530

    comments added

--- LeetCode/234. Palindrome Linked List.cpp
+++ LeetCode/234. Palindrome Linked List.cpp
@@ -1,25 +1,25 @@
 bool isPalindrome(ListNode *head)
 {
-
     if (head == NULL || head->next == NULL)
         return true;
 
     ListNode *slow = head;
     ListNode *fast = head;
 
+ //find the middle element , to reverse the next half
     while (fast->next != NULL && fast->next->next != NULL)
     {
         slow = slow->next;
         fast = fast->next->next;
     }
-
+    // revesing the next half for the comparision
     slow->next = reverse(slow->next);
     slow = slow->next;
-    ListNode *dummy = head;
+    ListNode *dummy = head; // now compare from start to mid, slow is in mid + 1 
 
     while (slow != NULL)
     {
-        if (dummy->val != slow->val)
+        if (dummy->val != slow->val) // if we found any value that is not equal we will show false.
             return false;
         dummy = dummy->next;
         slow = slow->next;
