a6609b38d0ab187da9b284572518029be3077896
Rishit <rsigotia123@gmail.com>
2024-11-07 00:37:52 +0530

    Minimum Platforms

---
+++ LeetCode/0. Minimum Platforms.cpp
@@ -0,0 +1,26 @@
+int findPlatform(vector<int>& arr, vector<int>& dep) 
+{
+       int count =0;
+       int max_count = 0;
+       int n = arr.size();
+       sort(arr.begin(),arr.end());
+       sort(dep.begin(),dep.end());
+       
+       int i = 0 , j = 0;
+       while(i < n)
+       {
+           if(arr[i] <= dep[j]) // the train is still there in the platform.
+           {
+               count++;
+               i++;
+           }
+           else
+           {
+               count--; // if arr is greater than departure of other train, then we don't have to worry.
+               j++;
+           }
+           max_count = max(max_count,count);
+       }
+       return max_count;
+       
+}
